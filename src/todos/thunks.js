e41e770efffa1388b3c1a01c6c81e36aa107b943
Rio <onlytru6340@gmail.com>
2024-09-21 20:43:34 -0700

    Async thunks

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,20 @@
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress()); 
+    const resposne = await fetch('https://localhost:8080/todos');
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure(todos));
+    dispatch(displayAlert(e));
+  }
+};
+
+
+
 export const displayAlert = text => () => {
-  alert(`You Clicked on ${text}`);
+  alert(text);
 };
