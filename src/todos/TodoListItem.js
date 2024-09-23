b33a582a9fe46b1650b0b788e86b8d8515c71c30
Rio <onlytru6340@gmail.com>
2024-09-23 00:56:00 -0700

    Test styled-components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -11,16 +11,13 @@ const TodoItemContainer = styled.div`
   box-shadow: 0 4px 8px grey;
 `;
 
-const TodoItemContainerWithWarning = styled(TodoItemContainer)`
-border-bottom: ${props => (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+export const getBorderStyleForDate = (startingDate, currentDate) = () => 
+    (startingDate > new Date(currentDate - 8640000 * 5)
       ? 'none'
-      : '2px solid red')};
-`;
+      : '2px solid red');
 
-const ButtonsContainer = styled.div`
-  position: absolute;
-  right: 12px;
-  bottom: 12px;
+const TodoItemContainerWithWarning = styled(TodoItemContainer)`
+border-bottom: ${props => getBorderStyleForDate(new Date(props.createdAt), Date.now())};
 `;
 
 const Button = styled.button`
---
+++ src/todos/test/TodoListItem.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { getBorderStyleForDate } from "../TodoListItem";
+
+describe('getBorderStyleForDate', () => {
+  it('returns none when the date is less than five days ago', () => {
+    const today = Date.now();
+    const recentDate = new Date(Date.now() - 8640000 * 7);
+
+    const expected = '2px solid red';
+    const actual = getBorderStyleForDate(recentDate, today);
+
+    expect(actual).to.deep.equal(expected);
+  });
+});
